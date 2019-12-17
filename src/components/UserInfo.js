ed6fb60b8d5d36bc219582017ff7edeaf28c571a
ElDoctorino <42263991+ElDoctorino@users.noreply.github.com>
2019-12-17 14:52:30 +0200

    2nd push

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,26 @@
 import React, {useState} from 'react';
 import '../App.css';
 
-const UserInfo =({showCart,setShowCart}) => {
+const UserInfo =({showCart,setShowCart,newInfo, submitHandler}) => {
 
     
         return(
-            <div>
-                <p> Hauska</p>
-            </div>
+
+                <div>
+                    <form onSubmit={submitHandler}>
+                        Nimi
+                        <input type="text" value={newInfo.nimi}  placeholder="nimi"/><br></br>
+                        email
+                        <input type="text" value={newInfo.email}  placeholder="email"/><br></br>
+                        osoite
+                        <input type="text" value={newInfo.osoite} placeholder="osoite"/><br></br>
+                        Postiosoite
+                        <input type="text" value={newInfo.Postiosoite}  placeholder="Postiosoite"/><br></br> 
+                        Postitoimipaikka
+                        <input type="text" value={newInfo.toimipaikka}  placeholder="Postitoimipaikka"/><br></br>
+                        <button type="submit">save</button>
+                        </form>
+                </div>
         )
         }
 export default UserInfo;
